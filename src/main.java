1f4c7b001781f6c8b3927c2736dff124ff77e0b3
HazemMagdyIsmail <hazemmagdyism@gmail.com>
2025-10-18 13:53:41 +0300

    del

--- src/main.java
+++ src/main.java
@@ -14,8 +14,7 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String name;
-        int num;
+        
     }
     
 }
