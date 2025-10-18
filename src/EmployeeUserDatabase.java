8ec0461bb6534fe36855cfc3adb35bb7d6d7a7d6
HazemMagdyIsmail <hazemmagdyism@gmail.com>
2025-10-18 20:57:22 +0300

    Update EmployeeUserDatabase.java

--- src/EmployeeUserDatabase.java
+++ src/EmployeeUserDatabase.java
@@ -1,3 +1,6 @@
+
+import java.io.FileNotFoundException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,16 @@
  *
  * @author hazem
  */
-public class EmployeeUserDatabase {
+public class EmployeeUserDatabase extends Database {
+
+    public EmployeeUserDatabase(String filename) throws FileNotFoundException {
+        super(filename);
+    }
     
+    @Override 
+   public EmployeeUser createRecordFrom (String line){
+   String[] words=line.trim().split(",");
+   return new EmployeeUser(words[0],words[1],words[2],words[3],words[4]);
+   
+   }
 }
