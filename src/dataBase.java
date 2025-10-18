ea6b40196bc221b1ca72f0a6d5425e60ef4f1a0b
patrickkfadyy <patrick.steven.fady@gmail.com>
2025-10-18 19:43:14 +0300

    Update dataBase.java

--- src/dataBase.java
+++ src/dataBase.java
@@ -7,30 +7,88 @@
  *
  * @author patrick
  */
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public abstract class dataBase {
-    String filename;
-    public abstract Record createRecordFrom(String line);
-    
-    public void readFromFile() throws FileNotFoundException{
-        File F=new File (filename);
-        Scanner s=new Scanner(F);
-        
-        ArrayList <Record> records=new ArrayList<>();
-        
-        while(s.hasNextLine()){
-            String x= s.nextLine();
-            records.add(createRecordFrom(x));
-        }
-        
-        
-        
+public abstract class Database {
+
+    private String filename;
+    private ArrayList<Record> recordsArray = new ArrayList<>();
+
+    public Database(String filename) throws FileNotFoundException {
+        this.filename = filename;
+        readFromFile();
     }
+
+    public abstract Record createRecordFrom(String line);
+
+    public void readFromFile() throws FileNotFoundException {
+        File F = new File(filename);
+        Scanner s = new Scanner(F);
+
+        while (s.hasNextLine()) {
+            String x = s.nextLine();
+            recordsArray.add(createRecordFrom(x));
+        }
+
+    }
+
+    public ArrayList<Record> returnAllRecords() {
+        return recordsArray;
+
+    }
+
+    public boolean contains(String key) {
+
+        for (int i = 0; i < recordsArray.size(); i++) {
+            if (key == recordsArray.get(i).getSearchKey()) {
+                return true;
+            }
+
+        }
+
+    }
+    public Record getRecord(String key){
+              for (int i = 0; i < recordsArray.size(); i++) {
+            if (key == recordsArray.get(i).getSearchKey()) {
+                return recordsArray.get(i);
+            }
+            
+
+        }
+              return null;
+    }
+    public void insertRecord(Record record){
+        recordsArray.add(record);
+    } 
+        public void deleteRecord(String key) {
+            for (int i = 0; i < recordsArray.size(); i++) {
+                 if (key == recordsArray.get(i).getSearchKey()) {
+                 recordsArray.remove(i);
+                 }
+                 
+                
+            }
+            
+            
+    }
+        public void saveToFile() throws FileNotFoundException{
+     PrintWriter writer = new PrintWriter(filename);
+    for(int i=0;i<recordsArray.size();i++){
+    String line = recordsArray.get(i).lineRepresentation();
+    writer.println(line);
+   }
+        writer.close();
     
-    
+
+
+
+   
+}
+
+
 }
