810feac1ed257b981baad4babfb444c2921cf581
El-hassanRamadan-debug <hassan1932005@gmail.com>
2025-10-18 19:07:58 +0300

    AdminRole,CustomerProduct

---
+++ src/CustomerProduct.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author El-hassan
+ */
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CustomerProduct {
+    private String customerSSN;
+    private String productID;
+    private LocalDate purchaseDate;
+    private boolean paid;
+
+   
+    public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
+        this.customerSSN = customerSSN;
+        this.productID = productID;
+        this.purchaseDate = purchaseDate;
+        this.paid = false;
+    }
+
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public String lineRepresentation() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        String formattedDate = purchaseDate.format(formatter);
+        return customerSSN + "," + productID + "," + formattedDate + "," + paid;
+    }
+
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
+
+    public String getSearchKey() {
+        return customerSSN + "," + productID;
+    }
+}
+
