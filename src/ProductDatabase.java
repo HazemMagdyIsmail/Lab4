b431ca501928e48fbfe1aadf351a3d6fbee4210b
HazemMagdyIsmail <hazemmagdyism@gmail.com>
2025-10-18 21:21:42 +0300

    Update ProductDatabase.java

--- src/ProductDatabase.java
+++ src/ProductDatabase.java
@@ -1,3 +1,6 @@
+
+import java.io.FileNotFoundException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +10,17 @@
  *
  * @author hazem
  */
-public class ProductDatabase {
+public class ProductDatabase extends Database {
+
+    public ProductDatabase(String filename) throws FileNotFoundException {
+        super(filename);
+    }
+    
+    @Override 
+   public Product createRecordFrom (String line){
+   String[] words=line.trim().split(",");
+   return new Product(words[0],words[1],words[2],words[3],Integer.parseInt(words[4]),Double.parseDouble(words[5]));
+   
+   }
     
 }
