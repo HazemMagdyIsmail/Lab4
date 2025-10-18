084ff2bba8cd92c9fd53e6355bd66a1b00b3b2d0
HazemMagdyIsmail <hazemmagdyism@gmail.com>
2025-10-18 14:34:27 +0300

    Create EmployeeUser.java

---
+++ src/EmployeeUser.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hazem
+ */
+public class EmployeeUser {
+    String employeeId;
+    String name;
+    String address;
+    String phoneNumber;
+}
